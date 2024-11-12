584b79c460582a5198b2cb286243bbd4a395450b
Mees R. <rasmees123@gmail.com>
2024-11-12 22:10:08 +0000

    Add comment option to admin

--- blog/models.py
+++ blog/models.py
@@ -12,3 +12,12 @@ class Post(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField(max_length=200)
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
